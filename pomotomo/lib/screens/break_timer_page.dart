18d0a78267274647fd172eff36e02707dbcb58a5
Bryant Hong <bhong4059@gmail.com>
2022-04-08 03:02:13 -0700

    Added break timer

---
+++ pomotomo/lib/screens/break_timer_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:simple_timer/simple_timer.dart';
+
+class break_timer_page extends StatefulWidget {
+  const break_timer_page({Key? key}) : super(key: key);
+
+  @override
+  State<break_timer_page> createState() => _break_timer_pageState();
+}
+
+const timerColor = Color(0xFFF16868);
+class _break_timer_pageState extends State<break_timer_page> with SingleTickerProviderStateMixin {
+
+  //TimerController? _timerController;
+  final TimerStyle _timerStyle = TimerStyle.ring;
+  final TimerProgressIndicatorDirection _progressIndicatorDirection = TimerProgressIndicatorDirection.clockwise;
+  final TimerProgressTextCountDirection _progressTextCountDirection = TimerProgressTextCountDirection.count_down;
+
+
+  Duration _workDuruation = const Duration(seconds: 5);
+
+  void exit(){
+    Navigator.pop(context, true);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(35),
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: SimpleTimer(
+                    duration: _workDuruation,
+                    //controller: _timerController,
+                    status: TimerStatus.start,
+                    timerStyle: TimerStyle.ring,
+                    progressIndicatorColor: timerColor,
+                    backgroundColor: Colors.blueGrey,
+                    progressIndicatorDirection: TimerProgressIndicatorDirection.clockwise,
+                    progressTextCountDirection: TimerProgressTextCountDirection.count_down,
+                    progressTextStyle: const TextStyle(fontSize: 65),
+                    strokeWidth: 10,
+                    onEnd: exit
+                ),
+              ),
+            ],
+          )
+        )
+    );
+
+
+  }
+}
--- pomotomo/lib/screens/simple_timer_page.dart
+++ pomotomo/lib/screens/simple_timer_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomotomo/screens/break_timer_page.dart';
 import 'package:simple_timer/simple_timer.dart';
 
 class simple_timer_page extends StatefulWidget {
@@ -14,9 +15,13 @@ class _simple_timer_pageState extends State<simple_timer_page> with SingleTicker
 
   // Simple Timer variables
   late TimerController _timerController;
+  late TimerController _timerController2;
   final TimerStyle _timerStyle = TimerStyle.ring;
   final TimerProgressIndicatorDirection _progressIndicatorDirection = TimerProgressIndicatorDirection.clockwise;
   final TimerProgressTextCountDirection _progressTextCountDirection = TimerProgressTextCountDirection.count_down;
+
+  Duration _workDuruation = const Duration(seconds: 2);
+  Duration _breakDuruation = const Duration(minutes: 5);
 
   @override
   void initState(){
@@ -29,6 +34,25 @@ class _simple_timer_pageState extends State<simple_timer_page> with SingleTicker
     _timerController.pause();
   }
 
+  void startbreakTimer(){
+    setState(() {
+      _workDuruation = _breakDuruation;
+      //const Duration(minutes: 5);
+    });
+    print(_workDuruation);
+  }
+
+  void setBreakTimer(){
+    _timerController.add(const Duration(minutes:5));
+    // setState(() {
+    //   _timerController.add(const Duration(minutes:5));
+    // });
+  }
+
+  void bye(){
+    Navigator.push(context, MaterialPageRoute(builder:(context) => const break_timer_page()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +63,7 @@ class _simple_timer_pageState extends State<simple_timer_page> with SingleTicker
                 padding: const EdgeInsets.all(35),
                 margin: const EdgeInsets.symmetric(vertical: 10),
                 child: SimpleTimer(
-                  duration: const Duration(minutes: 25),
+                  duration: _workDuruation,
                   controller: _timerController,
                   timerStyle: TimerStyle.ring,
                   progressIndicatorColor: timerColor,
@@ -48,6 +72,7 @@ class _simple_timer_pageState extends State<simple_timer_page> with SingleTicker
                   progressTextCountDirection: TimerProgressTextCountDirection.count_down,
                   progressTextStyle: const TextStyle(fontSize: 65),
                   strokeWidth: 10,
+                  onEnd: bye
                 ),
               ),
 
@@ -62,12 +87,36 @@ class _simple_timer_pageState extends State<simple_timer_page> with SingleTicker
                         child: const Text("Start"),
 
                     ),
+
+                    ElevatedButton(
+                        onPressed: () => _timerController.pause(),
+                        child: const Text("Pause"),
+                    ),
+
                     ElevatedButton(
                       onPressed: restartTimer,
                       child: const Text("Restart"),
 
                     ),
+
+                    ElevatedButton(
+                        onPressed: ()=> {
+                          setState(() {
+                            _timerController.add(const Duration(seconds:5));
+                          })
+                        },
+                        child: const Text("Add Time")
+                    ),
                   ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                        onPressed: (){},
+                        child: const Text("Start Break")
+                    )
+                  ]
 
                 ),
                 //Row of Buttons
@@ -78,6 +127,8 @@ class _simple_timer_pageState extends State<simple_timer_page> with SingleTicker
         ),
       ),
     );
+
+
   }
 
 }
